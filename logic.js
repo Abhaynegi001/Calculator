0f15a9a7fb41129aa5d5648e0c8c32bd45ae02f9
Abhaynegi001 <abhaynegi691.gmail.com>
2023-11-28 20:42:09 +0530

    change a input box

--- logic.js
+++ logic.js
@@ -1,14 +1,31 @@
 let input = document.getElementById('inputbox');
 let buttons = document.querySelectorAll('button');
+let result = document.querySelector(".result")
 let string = "";
+let num;
+
+console.log(result)
+
 let arr = Array.from(buttons);
 arr.forEach(button =>{
     button.addEventListener('click',(J)=>{
+        
         if(J.target.innerHTML=='='){
-            string = eval(string)
-            input.value= string
+            result.innerHTML = input.value
+            num = eval(string)
+            if(Number.isInteger(num)==true){
+                input.value= num
+            }
+            else{
+                string = num.toFixed(2)
+                input.value= string
+            }
         }
         else if(J.target.innerHTML=='AC'){
+            {
+                let str = string
+                result.innerHTML = `Ans = ${eval(str)}`
+            }
             string = ""
             input.value = string
         }
