56c1121acc310d0d22abee74485011986a57b249
Abhaynegi001 <abhaynegi691.gmail.com>
2023-11-29 21:46:15 +0530

    solve the problem of undefined

--- logic.js
+++ logic.js
@@ -1,43 +1,32 @@
-let input = document.getElementById('inputbox');
-let buttons = document.querySelectorAll('button');
-let result = document.querySelector(".result")
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll("button");
+let result = document.querySelector(".result");
 let string = "";
-
-
-console.log(result)
+let array = [];
 
 let arr = Array.from(buttons);
-arr.forEach(button =>{
-    button.addEventListener('click',(J)=>{
-        
-        if(J.target.innerHTML=='='){
-            result.innerHTML = input.value
-            string = eval(string)
-            if(Number.isInteger(string)==true){
-                input.value= string
-            }
-            else{
-                string = string.toFixed(2)
-                input.value= string
-            }
-        }
-        else if(J.target.innerHTML=='AC'){
-            {
-                let str = string
-                result.innerHTML = `Ans = ${eval(str)}`
-            }
-            string = ""
-            input.value = string
-        }
-        else if(J.target.innerHTML=='DEL'){  
-            
-            string = string.substring(0,string.length-1)
-            input.value=string
-            
-        }
-        else{
-            string+= J.target.innerHTML;
-            input.value = string
-        }
-    })
-})
+arr.forEach((button) => {
+  button.addEventListener("click", (J) => {
+    if (J.target.innerHTML == "=") {
+      result.innerHTML = input.value;
+      string = eval(string);
+      array.push(string);
+      if (Number.isInteger(string) == true) {
+        input.value = string;
+      } else {
+        string = string.toFixed(2);
+        input.value = string;
+      }
+    } else if (J.target.innerHTML == "AC") {
+      result.innerHTML = `Ans = ${array[array.length - 1]}`;
+      string = "";
+      input.value = string;
+    } else if (J.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += J.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
